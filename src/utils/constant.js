77ecae4c401a682ab803915f7c8b18f84c593a86
fuyongjie <15052418028@139.com>
2020-04-08 22:37:57 +0800

    修改仓库CRUD

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -12,7 +12,7 @@ export function danWeiConstant() {
 // 灵物分类
 export function lingWuFenLeiConstant() {
   const lingWuFenLei = [
-    '妖兽', '符箓', '功法', '灵材', '傀儡', '法术', '法器', '其他灵物', '灵丹', '武佛道儒魔妖'
+    '妖兽', '符箓', '功法', '灵材', '傀儡', '法术', '法器', '其他灵物', '灵丹', '人神妖魔鬼怪'
   ].map(data => ({ dataCode: data, dataName: data }));
   return lingWuFenLei;
 }
@@ -22,7 +22,7 @@ export function jiaGouFenLeiConstant() {
   const jiaGouFenLei = [
     '散修', '散修联盟', '散修联盟机构', '修仙家族', '家族弟子', '家族机构',
     '修仙门派', '门派弟子', '门派机构', '修仙组织', '组织弟子', '组织机构',
-    '修仙机构', '修仙者', '修仙圣地', '圣地修行者', '圣地机构',
+    '修仙机构', '修仙者', '修仙圣地', '圣地修仙者', '圣地机构',
   ].map(data => ({ dataCode: data, dataName: data }));
   return jiaGouFenLei;
 }
