667a2d051e84e0959d8b7ad362cf5cb6c946e7bb
fuyongjie <15052418028@139.com>
2020-06-16 00:01:14 +0800

    sheding,fulu,zongmen

--- src/models/fuLu.js
+++ src/models/fuLu.js
@@ -1,62 +1,10 @@
-import { queryFuLu, getFuLu, saveFuLu, updateFuLu, delFuLu } from '../services/fuLu';
+import {createModel} from "../utils/metaModel";
+
 const Model = {
   namespace: 'fuLu',
-  state: {
-    datas: {
-      list: [],
-      pagination:{
-        current: 1,
-        pageSize: 10,
-      }
-    },
-    data: {},
-  },
-  effects: {
-    *query({ payload }, { call, put }) {
-      const response = yield call(queryFuLu, payload);
-      yield put({
-        type: 'save',
-        payload: response,
-      });
-    },
-    *get({ payload }, { call, put }) {
-      const response = yield call(getFuLu, payload);
-      yield put({
-        type: 'change',
-        payload: response,
-      });
-    },
-    *add({ payload }, { call, put }) {
-      const response = yield call(saveFuLu, payload);
-      yield put({
-        type: 'change',
-        payload: response,
-      });
-    },
-    *update({ payload }, { call, put }) {
-      const response = yield call(updateFuLu, payload);
-      yield put({
-        type: 'change',
-        payload: response,
-      });
-    },
-    *del({ payload }, { call, put }) {
-      yield call(delFuLu, payload);
-    },
-  },
-  reducers: {
-    save(state, { payload }) {
-      return { ...state, datas: payload };
-    },
-    change(state, { payload }) {
-      return { ...state, data: payload };
-    },
-    emptyProfile(state) {
-      return {
-        ...state,
-        data: {},
-      };
-    },
-  },
 };
+const metaModel = createModel(Model.namespace);
+Model.state = Object.assign({}, metaModel.state, Model.state);
+Model.reducers = Object.assign({}, metaModel.reducers, Model.reducers);
+Model.effects = Object.assign({}, metaModel.effects, Model.effects);
 export default Model;
--- src/models/zongMen.js
+++ src/models/zongMen.js
@@ -1,63 +1,10 @@
-import { queryZongMen, getZongMen, saveZongMen, updateZongMen, delZongMen } from '../services/zongMen';
+import {createModel} from "../utils/metaModel";
 
 const Model = {
   namespace: 'zongMen',
-  state: {
-    datas: {
-      list: [],
-      pagination:{
-        current: 1,
-        pageSize: 10,
-      }
-    },
-    data: {},
-  },
-  effects: {
-    *query({ payload }, { call, put }) {
-      const response = yield call(queryZongMen, payload);
-      yield put({
-        type: 'save',
-        payload: response,
-      });
-    },
-    *get({ payload }, { call, put }) {
-      const response = yield call(getZongMen, payload);
-      yield put({
-        type: 'change',
-        payload: response,
-      });
-    },
-    *add({ payload }, { call, put }) {
-      const response = yield call(saveZongMen, payload);
-      yield put({
-        type: 'change',
-        payload: response,
-      });
-    },
-    *update({ payload }, { call, put }) {
-      const response = yield call(updateZongMen, payload);
-      yield put({
-        type: 'change',
-        payload: response,
-      });
-    },
-    *del({ payload }, { call }) {
-      yield call(delZongMen, payload);
-    },
-  },
-  reducers: {
-    save(state, { payload }) {
-      return { ...state, datas: payload };
-    },
-    change(state, { payload }) {
-      return { ...state, data: payload };
-    },
-    emptyProfile(state) {
-      return {
-        ...state,
-        data: {},
-      };
-    },
-  },
 };
+const metaModel = createModel(Model.namespace);
+Model.state = Object.assign({}, metaModel.state, Model.state);
+Model.reducers = Object.assign({}, metaModel.reducers, Model.reducers);
+Model.effects = Object.assign({}, metaModel.effects, Model.effects);
 export default Model;
